0b0059671d3f26a920c57a9daf5a440f00cc4e88
Amy <amy.platt@hotmail.com>
2026-01-08 15:22:15 +0200

    Add stories to storiesbar component

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { dummyPostsData } from "../assets/assets";
 import Loading from "../components/Loading";
+import StoriesBar from "../components/StoriesBar";
 
 const Feed = () => {
   const [feeds, setFeeds] = useState([]);
@@ -19,7 +20,7 @@ const Feed = () => {
     <div className="h-full overflow-y-scroll no-scrollbar py-10 xl:pr-5 flex items-start justify-center xl:gap-8">
       {/* Stories and post list */}
       <div>
-        <h1>Stories here</h1>
+        <StoriesBar />
         <div className="p-4 space-y-6">List of posts</div>
       </div>
       {/* Right sidebar */}
