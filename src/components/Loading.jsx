e88d7b73187016d4eb5ab1fb5fc90a70e66eb82c
Amy <amy.platt@hotmail.com>
2026-01-08 13:46:51 +0200

    Add menuitems and layout

---
+++ src/components/Loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = ({height = '100vh'}) => {
+  return (
+    <div className='flex items-center justify-center h-screen'>
+        <div style={{height}} className='w-10 h-10 rpunded-full border-3 border-purple-500 border-t-transparent animate-spin'></div>
+    </div>
+  );
+};
+
+export default Loading;
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import { Menu, X } from "lucide-react";
+import React, { useState } from "react";
+import { Outlet } from "react-router-dom";
+import { dummyUserData } from "../assets/assets";
+import Loading from "../components/Loading";
+import Sidebar from "../components/Sidebar";
 
 const Layout = () => {
-  return (
-    <div>
-      <h1>Layout</h1>
+  const user = dummyUserData;
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  return user ? (
+    <div className="w-full flex h-screen">
+      <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+      <div className="flex-1 bg-slate-50">
+        <Outlet />
+      </div>
+      {sidebarOpen ? (
+        <X
+          className="absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow w-10 text-gray-600 sm:hidden"
+          onClick={() => setSidebarOpen(false)}
+        />
+      ) : (
+        <Menu
+          className="absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow w-10 text-gray-600 sm:hidden"
+          onClick={() => setSidebarOpen(true)}
+        />
+      )}
     </div>
+  ) : (
+    <Loading />
   );
 };
 
 export default Layout;
+
+/* https://www.youtube.com/watch?v=7oKFzriZt0A&list=PLir6UUEqD4JGojD_EL_E9l8WHpVDfnAmd&index=28 56:41 */
