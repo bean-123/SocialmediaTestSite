57bf54ae3e3cf04061ed2f042de6aac6c516bcae
Amy <amy.platt@hotmail.com>
2026-01-08 14:04:38 +0200

    Add create post button on sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { assets } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
 import MenuItems from "./MenuItems";
+import { CirclePlus, Link as LinkIcon } from "lucide-react";
+import { Link } from "react-router-dom";
 
 const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
   const navigate = useNavigate();
@@ -17,11 +19,20 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
           onClick={() => navigate("/")}
           src={assets.logo}
           className="w-26 ml-7 my-2 cursor-pointer"
-          alt=""
         />
         <hr className="border-gray-300 mb-8" />
 
         <MenuItems setSidebarOpen={setSidebarOpen} />
+
+        <Link
+          to="/create-post"
+          className="flex items-center justify-center gap-2 py-2.5 mt-6 mx-6 rounded-lg 
+          bg-gradient-to-r from-indigo-500 to-purple-600 hover:from-indigo-700 
+          hover:to-purple-800 active:scale-95 transition text-white cursor-pointer"
+        >
+          <CirclePlus className="w-5 h-5" />
+          Create Post
+        </Link>
       </div>
     </div>
   );
